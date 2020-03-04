5c7cb5c9d373702bb39e562412b9b9f6d70d9178
hiCasper <hiCasper@users.noreply.github.com>
2020-03-05 00:36:55 +0800

    Update AutoReinstall.sh

--- AutoReinstall.sh
+++ AutoReinstall.sh
@@ -6,6 +6,23 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+function CopyRight() {
+  clear
+  echo "########################################################"
+  echo "#                                                      #"
+  echo "#  Auto Reinstall Script                               #"
+  echo "#                                                      #"
+  echo "#  Author: hiCasper                                    #"
+  echo "#  Blog: blog.hicasper.com/post/135.html               #"
+  echo "#  Feedback: https://github.com/hiCasper/Shell/issues  #"
+  echo "#  Last Modified: 2020-02-19                           #"
+  echo "#                                                      #"
+  echo "#  Supported by MoeClub                                #"
+  echo "#                                                      #"
+  echo "########################################################"
+  echo -e "\n"
+}
+
 function isValidIp() {
   local ip=$1
   local ret=1
@@ -15,13 +32,6 @@ function isValidIp() {
     ret=$?
   fi
   return $ret
-}
-
-function updateIp() {
-  CopyRight
-  read -r -p "Your IP: " MAINIP
-  read -r -p "Your Gateway: " GATEWAYIP
-  read -r -p "Your Netmask: " NETMASK
 }
 
 function ipCheck() {
@@ -35,28 +45,129 @@ function ipCheck() {
   return $isLegal
 }
 
-function CopyRight() {
-  clear
-  echo "########################################################"
-  echo "#                                                      #"
-  echo "#  Auto Reinstall Script                               #"
-  echo "#                                                      #"
-  echo "#  Author: hiCasper                                    #"
-  echo "#  Blog: blog.hicasper.com/post/135.html               #"
-  echo "#  Feedback: https://github.com/hiCasper/Shell/issues  #"
-  echo "#  Last Modified: 2020-02-18                           #"
-  echo "#                                                      #"
-  echo "#  Supported by MoeClub                                #"
-  echo "#                                                      #"
-  echo "########################################################"
-  echo -e "\n"
+function GetIp() {
+  MAINIP=$(ip route get 1 | awk -F 'src ' '{print $2}' | awk '{print $1}')
+  GATEWAYIP=$(ip route | grep default | awk '{print $3}')
+  SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/",$4);print $4}' | head -1 | awk -F '/' '{print $2}')
+  value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
+  NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
 }
 
-function start() {
+function UpdateIp() {
+  read -r -p "Your IP: " MAINIP
+  read -r -p "Your Gateway: " GATEWAYIP
+  read -r -p "Your Netmask: " NETMASK
+}
+
+function SetNetwork() {
+  isAuto='0'
+  if [[ -f '/etc/network/interfaces' ]];then
+    [[ ! -z "$(sed -n '/iface.*inet static/p' /etc/network/interfaces)" ]] && isAuto='1'
+    [[ -d /etc/network/interfaces.d ]] && {
+      cfgNum="$(find /etc/network/interfaces.d -name '*.cfg' |wc -l)" || cfgNum='0'
+      [[ "$cfgNum" -ne '0' ]] && {
+        for netConfig in `ls -1 /etc/network/interfaces.d/*.cfg`
+        do 
+          [[ ! -z "$(cat $netConfig | sed -n '/iface.*inet static/p')" ]] && isAuto='1'
+        done
+      }
+    }
+  fi
+  
+  if [[ -d '/etc/sysconfig/network-scripts' ]];then
+    cfgNum="$(find /etc/network/interfaces.d -name '*.cfg' |wc -l)" || cfgNum='0'
+    [[ "$cfgNum" -ne '0' ]] && {
+      for netConfig in `ls -1 /etc/sysconfig/network-scripts/ifcfg-* | grep -v 'lo$' | grep -v ':[0-9]\{1,\}'`
+      do 
+        [[ ! -z "$(cat $netConfig | sed -n '/BOOTPROTO.*[sS][tT][aA][tT][iI][cC]/p')" ]] && isAuto='1'
+      done
+    }
+  fi
+}
+
+function NetMode() {
   CopyRight
-  echo "IP: $MAINIP"
-  echo "Gateway: $GATEWAYIP"
-  echo "Netmask: $NETMASK"
+
+  if [ "$isAuto" == '0' ]; then
+    read -r -p "Using DHCP to configure network automatically? [Y/n]:" input
+    case $input in
+      [yY][eE][sS]|[yY]) NETSTR='' ;;
+      [nN][oO]|[nN]) isAuto='1' ;;
+      *) clear; echo "Canceled by user!"; exit 1;;
+    esac
+  fi
+
+  if [ "$isAuto" == '1' ]; then
+    GetIp
+    ipCheck
+    if [ $? -ne 0 ]; then
+      echo -e "Error occurred when detecting ip. Please input manually.\n"
+      UpdateIp
+    else
+      CopyRight
+      echo "IP: $MAINIP"
+      echo "Gateway: $GATEWAYIP"
+      echo "Netmask: $NETMASK"
+      echo -e "\n"
+      read -r -p "Confirm? [Y/n]:" input
+      case $input in
+        [yY][eE][sS]|[yY]) ;;
+        [nN][oO]|[nN])
+          echo -e "\n"
+          UpdateIp
+          ipCheck
+          [[ $? -ne 0 ]] && {
+            clear
+            echo -e "Input error!\n"
+            exit 1
+          }
+        ;;
+        *) clear; echo "Canceled by user!"; exit 1;;
+      esac
+    fi
+    NETSTR="--ip-addr ${MAINIP} --ip-gate ${GATEWAYIP} --ip-mask ${NETMASK}"
+  fi
+}
+
+function Start() {
+  CopyRight
+  
+  isCN='0'
+  geoip=$(wget --no-check-certificate -qO- https://api.ip.sb/geoip | grep "\"country_code\":\"CN\"")
+  if [[ "$geoip" != "" ]];then
+    isCN='1'
+  fi
+
+  if [ "$isAuto" == '0' ]; then
+    echo "Using DHCP mode."
+  else
+    echo "IP: $MAINIP"
+    echo "Gateway: $GATEWAYIP"
+    echo "Netmask: $NETMASK"
+  fi
+
+  [[ "$isCN" == '1' ]] && echo "Using domestic mode."
+
+  if [ -f "/tmp/InstallNET.sh" ]; then
+    rm -f /tmp/InstallNET.sh
+  fi
+  wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
+  
+  CMIRROR=''
+  CVMIRROR=''
+  DMIRROR=''
+  UMIRROR=''
+  if [[ "$isCN" == '1' ]];then
+    sed -i 's#https://moeclub.org/get/wget_udeb_amd64#https://api.moetools.net/get/wget_udeb_amd64#' /tmp/InstallNET.sh
+    CMIRROR="--mirror http://mirrors.aliyun.com/centos/"
+    CVMIRROR="--mirror http://mirrors.tuna.tsinghua.edu.cn/centos-vault/"
+    DMIRROR="--mirror http://mirrors.aliyun.com/debian/"
+    UMIRROR="--mirror http://mirrors.aliyun.com/ubuntu/"
+  fi
+  
+  sed -i 's/$1$4BJZaD0A$y1QykUnJ6mXprENfwpseH0/$1$7R4IuxQb$J8gcq7u9K0fNSsDNFEfr90/' /tmp/InstallNET.sh
+  sed -i '/force-efi-extra-removable/d' /tmp/InstallNET.sh
+
   echo -e "\nPlease select an OS:"
   echo "  1) CentOS 7.7 (DD Image)"
   echo "  2) CentOS 7.6 (ServerSpeeder Avaliable)"
@@ -70,20 +181,20 @@ function start() {
   echo -ne "\nYour option: "
   read N
   case $N in
-    1) echo -e "\nPassword: Pwd@CentOS\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK -dd 'https://api.moetools.net/get/centos-7-image' $DMIRROR ;;
-    2) echo -e "\nPassword: Pwd@CentOS\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK -dd 'https://api.moetools.net/get/centos-76-image' $DMIRROR ;;
-    3) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -c 6.10 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK $CMIRROR ;;
-    4) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -d 9 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK $DMIRROR ;;
-    5) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -d 10 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK $DMIRROR ;;
-    6) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -u 16.04 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK $UMIRROR ;;
-    7) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -u 18.04 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK $UMIRROR ;;
+    1) echo -e "\nPassword: Pwd@CentOS\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh $NETSTR -dd 'https://api.moetools.net/get/centos-7-image' $DMIRROR ;;
+    2) echo -e "\nPassword: Pwd@CentOS\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh $NETSTR -dd 'https://api.moetools.net/get/centos-76-image' $DMIRROR ;;
+    3) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -c 6.10 -v 64 -a $NETSTR $CMIRROR ;;
+    4) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -d 9 -v 64 -a $NETSTR $DMIRROR ;;
+    5) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -d 10 -v 64 -a $NETSTR $DMIRROR ;;
+    6) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -u 16.04 -v 64 -a $NETSTR $UMIRROR ;;
+    7) echo -e "\nPassword: Pwd@Linux\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -u 18.04 -v 64 -a $NETSTR $UMIRROR ;;
     8)
       echo -e "\n"
       read -r -p "Custom image URL: " imgURL
       echo -e "\n"
       read -r -p "Are you sure start reinstall? [Y/n]: " input
       case $input in
-        [yY][eE][sS]|[yY]) bash /tmp/InstallNET.sh --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK -dd $imgURL $DMIRROR ;;
+        [yY][eE][sS]|[yY]) bash /tmp/InstallNET.sh $NETSTR -dd $imgURL $DMIRROR ;;
         *) clear; echo "Canceled by user!"; exit 1;;
       esac
       ;;
@@ -92,70 +203,6 @@ function start() {
   esac
 }
 
-CMIRROR=''
-CVMIRROR=''
-DMIRROR=''
-UMIRROR=''
-isCN='0'
-
-geoip=$(wget --no-check-certificate -qO- https://api.ip.sb/geoip | grep "\"country_code\":\"CN\"")
-if [[ "$geoip" != "" ]];then
-  isCN='1'
-fi
-
-if [ -f "/tmp/InstallNET.sh" ]; then
-  rm -f /tmp/InstallNET.sh
-fi
-wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
-
-if [[ "$isCN" == '1' ]];then
-  sed -i 's#https://moeclub.org/get/wget_udeb_amd64#https://api.moetools.net/get/wget_udeb_amd64#' /tmp/InstallNET.sh
-  CMIRROR="--mirror http://mirrors.aliyun.com/centos/"
-  CVMIRROR="--mirror http://mirrors.tuna.tsinghua.edu.cn/centos-vault/"
-  DMIRROR="--mirror http://mirrors.aliyun.com/debian/"
-  UMIRROR="--mirror http://mirrors.aliyun.com/ubuntu/"
-fi
-
-sed -i 's/$1$4BJZaD0A$y1QykUnJ6mXprENfwpseH0/$1$7R4IuxQb$J8gcq7u9K0fNSsDNFEfr90/' /tmp/InstallNET.sh
-sed -i '/force-efi-extra-removable/d' /tmp/InstallNET.sh
-
-MAINIP=$(ip route get 1 | awk -F 'src ' '{print $2}' | awk '{print $1}')
-GATEWAYIP=$(ip route | grep default | awk '{print $3}')
-SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/",$4);print $4}' | head -1 | awk -F '/' '{print $2}')
-value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
-NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
-
-ipCheck
-if [ $? -eq 0 ]; then
-  CopyRight
-  echo "IP: $MAINIP"
-  echo "Gateway: $GATEWAYIP"
-  echo "Netmask: $NETMASK"
-  if [[ "$isCN" == '1' ]];then
-    echo "Using domestic mode."
-  fi
-  echo -ne "\n"
-  read -r -p "Please confirm your network infomation [Y/n]: " input
-  case $input in
-    [yY][eE][sS]|[yY]) start;;
-    [nN][oO]|[nN])
-      updateIp
-      ipCheck
-      if [ $? -eq 0 ]; then
-        start
-      else
-        echo -e "\nIllegal address!"; exit 1
-      fi
-      ;;
-    *) echo "Wrong input!"; exit 1;;
-  esac
-else
-  echo -e "\nIllegal address!"
-  updateIp
-  ipCheck
-  if [ $? -eq 0 ]; then
-    start
-  else
-    echo -e "\nIllegal address!"
-  fi
-fi
+SetNetwork
+NetMode
+Start
