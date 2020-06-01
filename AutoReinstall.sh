4232fceed8a5f0b7c512afea1202474400676e09
hiCasper <hiCasper@users.noreply.github.com>
2020-06-01 20:15:35 +0800

    Fix error on Debian

--- AutoReinstall.sh
+++ AutoReinstall.sh
@@ -15,7 +15,7 @@ function CopyRight() {
   echo "#  Author: hiCasper                                    #"
   echo "#  Blog: blog.hicasper.com/post/135.html               #"
   echo "#  Feedback: https://github.com/hiCasper/Shell/issues  #"
-  echo "#  Last Modified: 2020-04-30                           #"
+  echo "#  Last Modified: 2020-06-01                           #"
   echo "#                                                      #"
   echo "#  Supported by MoeClub                                #"
   echo "#                                                      #"
@@ -47,7 +47,7 @@ function ipCheck() {
 
 function GetIp() {
   MAINIP=$(ip route get 1 | awk -F 'src ' '{print $2}' | awk '{print $1}')
-  GATEWAYIP=$(ip route | grep default | awk '{print $3}; exit')
+  GATEWAYIP=$(ip route | grep default | awk '{print $3}' | head -1)
   SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/",$4);print $4}' | head -1 | awk -F '/' '{print $2}')
   value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
   NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
