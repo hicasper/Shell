2002253b5c827f1683edbb6cc2fac96b03943304
hiCasper <hiCasper@users.noreply.github.com>
2022-01-05 00:56:34 +0800

    Update

--- AppNodeUpdateCerts.php
+++ AppNodeUpdateCerts.php
@@ -5,37 +5,49 @@
 require_once('Medoo.php');
 use Medoo\Medoo;
 
-$siteid = array(1,2);  // 'siteid' in sitemgr.db
+$sitecode = array('sitecode1', 'sitecode2');  // 'site_code' in sitemgr.db (网站代号)
 $certpath = '/data/ssl';
 $dbpath = '/opt/appnode/agent/apps/sitemgr/db/sitemgr.db';
-$certname = array('*.example.com', 'example.com');   // folder name in Let's Encrypt home directory
+$certname = array('*.example.com', 'example.com');  // folder name in Let's Encrypt home directory
+
+$enable_nginx_cert_replace = true;
+
+$ngxcertdir = array('/path/to/cert', '/path/to/cert');   // nginx cert path
 
 $db = new Medoo([
     'type' => 'sqlite',
     'database' => $dbpath
 ]);
 
-$size = count($siteid);
+$size = count($sitecode);
 for ($i=0;$i<$size;$i++)
 {
     $data = $db -> get("site",[
         "setting"
     ],[
-        "site_id" => $siteid[$i]
+        "site_code" => $sitecode[$i]
     ]);
 
     $data = json_decode($data["setting"]);
+    
+    $newkey = file_get_contents("$certpath/$certname[$i]/$certname[$i].key");
+    $newcert = file_get_contents("$certpath/$certname[$i]/fullchain.cer");
 
-    $data -> SSLKeyPemCode = file_get_contents("$certpath/$certname[$i]/$certname[$i].key");
-    $data -> SSLCertPemCode = file_get_contents("$certpath/$certname[$i]/fullchain.cer");
+    $data -> SSLKeyPemCode = $newkey;
+    $data -> SSLCertPemCode = $newcert;
 
     $data = json_encode($data);
 
     $data = $db -> update("site",[
         "setting" => $data
     ],[
-        "site_id" => $siteid[$i]
+        "site_code" => $sitecode[$i]
     ]);
+    
+    if ($enable_nginx_cert_replace && $ngxcertdir[$i] != '') {
+        file_put_contents("$ngxcertdir[$i]/site.key", $newkey);
+        file_put_contents("$ngxcertdir[$i]/site.crt", $newcert);
+    }
 }
 
 echo 'Done!';
